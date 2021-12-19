3fbd185c845a08904f84629d829dae8c41bfe43a
Jire <Rohit.Jire@t-systems.com>
2021-12-19 13:18:20 +0530

    initial commit

---
+++ src/test/java/com/example/bankingassignment/BankingAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bankingassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
